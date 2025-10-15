68258342ef02c5b2bf2db6efadd64bdef77b758c
atheer <atheermoha17@gmail.com>
2025-10-15 20:51:05 +0300

    Initial commit

---
+++ src/test/java/assignment/T03_RestAssuredAPIResponseValidation.java
@@ -0,0 +1,26 @@
+package assignment;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class T03_RestAssuredAPIResponseValidation {
+
+        @Test
+        void validateAPIResponse() {
+            Response response = RestAssured.get("https://fakerestapi.azurewebsites.net/api/v1/Users");
+
+            System.out.println("response statusCode = " + response.statusCode());
+            System.out.println("response Headers = " + response.getHeaders());
+            System.out.println("response.getHeader(Server) = " + response.getHeader("Server"));
+            System.out.println("response.getHeader(Transfer-Encoding) = " + response.getHeader("Transfer-Encoding"));
+
+            //validate
+            Assert.assertEquals(response.statusCode(),200);
+            Assert.assertTrue(response.getContentType().contains("application/json"));
+            Assert.assertTrue(response.getHeader("Server").contains("Kestrel"));
+            Assert.assertEquals(response.getHeader("Transfer-Encoding"),"chunked");
+        }
+    }
+
