8bae6bbf3ba67b9ccc1b321c86675250b8b26f8a
atheer <atheermoha17@gmail.com>
2025-10-22 15:52:50 +0300

    day 5 tasks

--- src/test/java/day05/T01_GoREST/partialUpdateUser_05.java
+++ src/test/java/day05/T01_GoREST/partialUpdateUser_05.java
@@ -1,6 +1,35 @@
 package day05.T01_GoREST;
 
 import baseUrl.goRestBaseUrl;
+import com.fasterxml.jackson.databind.JsonNode;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static day05.T01_GoREST.createUser_02.userId;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static utilities.ObjectMapperUtils.getJsonNode;
 
 public class partialUpdateUser_05 extends goRestBaseUrl {
+
+    @Test
+    void partialupdate(){
+
+        //update by use new json file
+        JsonNode payload =getJsonNode("T01_createUser(partialupdate)");
+
+        //send request
+        Response response = given(spec).body(payload).patch("/users/"+userId);
+        response.prettyPrint();
+
+        //the rest body from previous file
+        JsonNode updatedJson=getJsonNode("T01_createUser");
+
+        //do assertions
+        response.then().statusCode(200)
+                .body("email",equalTo(payload.get("email").asText()),
+                        "status",equalTo(payload.get("status").asText()));
+    }
+
+
 }
--- src/test/java/day05/T01_GoREST/updateUser_04.java
+++ src/test/java/day05/T01_GoREST/updateUser_04.java
@@ -1,6 +1,33 @@
 package day05.T01_GoREST;
 
 import baseUrl.goRestBaseUrl;
+import com.fasterxml.jackson.databind.JsonNode;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static day05.T01_GoREST.createUser_02.userId;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static utilities.ObjectMapperUtils.getJsonNode;
 
 public class updateUser_04 extends goRestBaseUrl {
+
+
+    @Test
+    void updateUser(){
+
+        //update by use new json file
+        JsonNode payload =getJsonNode("T01_createUser(updated)");
+
+        //send request
+        Response response = given(spec).body(payload).put("/users/"+userId);
+        response.prettyPrint();
+
+        //do assertions
+        response.then().statusCode(200)
+                .body("name",equalTo(payload.get("name").asText()),
+                        "gender",equalTo(payload.get("gender").asText()),
+                        "email",equalTo(payload.get("email").asText()),
+                        "status",equalTo(payload.get("status").asText()));
+    }
 }
