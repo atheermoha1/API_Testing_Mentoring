a8bd3db8d0569b2726ed00c5c892354d2516306a
atheer <atheermoha17@gmail.com>
2025-10-22 16:09:58 +0300

    day 5 tasks

--- src/test/java/day05/T01_GoREST/deleteUser_06.java
+++ src/test/java/day05/T01_GoREST/deleteUser_06.java
@@ -1,6 +1,27 @@
 package day05.T01_GoREST;
 
 import baseUrl.goRestBaseUrl;
+import com.fasterxml.jackson.databind.JsonNode;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static day05.T01_GoREST.createUser_02.userId;
+import static io.restassured.RestAssured.given;
+import static utilities.ObjectMapperUtils.getJsonNode;
 
 public class deleteUser_06 extends goRestBaseUrl {
+
+    @Test
+    void deleteUser(){
+
+
+        //send request
+        Response response = given(spec).delete("/users/"+userId);
+        response.prettyPrint();
+
+        response.then().statusCode(204);
+
+
+
+    }
 }
